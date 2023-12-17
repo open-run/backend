a17c8d028f295c68c10f3d581dc368285897f8f4
Yejin Kelly Joo <yejinkellyjoo@gmail.com>
2023-12-17 20:50:26 +0900

    init repo

---
+++ src/main/java/io/openur/OpenurApplication.java
@@ -0,0 +1,13 @@
+package io.openur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/openur/OpenurApplicationTests.java
@@ -0,0 +1,13 @@
+package io.openur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
