52e670273b591b824253416222a9253a9bddf5fc
Yejin Kelly Joo <yjkellyjoo@pi-lab.io>
2023-12-26 21:39:55 +0900

    BE-2, chore: comment reason

--- src/test/java/io/openur/OpenurApplicationTests.java
+++ src/test/java/io/openur/OpenurApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OpenurApplicationTests {
 
+//    TODO: replace with real test code. commented out for docker build.
 //    @Test
 //    void contextLoads() {
 //    }
