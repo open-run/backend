3e3ccc96f4aa8f941ddb916a9d8e4c1c91e48145
Yejin Kelly Joo <yejinkellyjoo@gmail.com>
2024-02-13 22:23:00 +0900

    BE-12, feature: change database from postgres to mysql
    Signed-off-by: Yejin Kelly Joo <yejinkellyjoo@gmail.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,43 +1,33 @@
--- Create ENUM type for provider if not exists
-DO
-'BEGIN
-  IF (NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = ''provider_enum''))
-  THEN
-    CREATE TYPE provider_enum AS ENUM (''GOOGLE'', ''NAVER'', ''KAKAO'');
-  END IF;
-END;
-';
-
--- Create table TB_USERS if not exists
-CREATE TABLE IF NOT EXISTS tb_users (
-                                      user_id BIGINT PRIMARY KEY NOT NULL,
+-- tb_users 테이블 생성 (존재하지 않을 경우에만)
+CREATE TABLE IF NOT EXISTS TB_USERS (
+                                      user_id BIGINT(20) PRIMARY KEY NOT NULL,
                                       withdraw BOOLEAN NOT NULL DEFAULT FALSE,
                                       nickname VARCHAR(16) NOT NULL,
-                                      email VARCHAR(255) NOT NULL CHECK (email ~ '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$'),
+                                      email VARCHAR(255) NOT NULL,
                                       identity_authenticated BOOLEAN NOT NULL DEFAULT FALSE,
-                                      provider provider_enum NOT NULL,
+                                      provider ENUM('GOOGLE', 'NAVER', 'KAKAO') NOT NULL,
                                       blacklisted BOOLEAN NOT NULL DEFAULT FALSE,
                                       created_date TIMESTAMP NOT NULL,
                                       last_login_date TIMESTAMP NOT NULL,
                                       blockchain_address VARCHAR(42) NOT NULL
 );
 
--- Create table TB_WITHDRAWS if not exists
-CREATE TABLE IF NOT EXISTS tb_withdraws (
-                                          user_id BIGINT PRIMARY KEY NOT NULL,
-                                          deferment_period INTEGER CHECK (deferment_period IN (15, 30, 60)),
+-- tb_withdraws 테이블 생성 (존재하지 않을 경우에만)
+CREATE TABLE IF NOT EXISTS TB_WITHDRAWS (
+                                          user_id BIGINT(20) PRIMARY KEY NOT NULL,
+                                          deferment_period ENUM('15', '30', '60') NOT NULL DEFAULT '15',
                                           created_date TIMESTAMP NOT NULL,
-                                          CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES tb_users(user_id)
+                                          CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES TB_USERS(user_id)
 );
 
--- Create table TB_BUNG if not exists
-CREATE TABLE IF NOT EXISTS tb_bung (
-                                     bung_id UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+-- tb_bung 테이블 생성 (존재하지 않을 경우에만)
+CREATE TABLE IF NOT EXISTS TB_BUNG (
+                                     bung_id VARCHAR(36) DEFAULT UUID() PRIMARY KEY NOT NULL,
                                      location VARCHAR(128) NOT NULL DEFAULT '',
-                                     datetime TIMESTAMP DEFAULT '1970-01-01 00:00:00' NOT NULL,
+                                     datetime TIMESTAMP DEFAULT 0 NOT NULL,
                                      bung_name VARCHAR(192) NOT NULL DEFAULT '',
-                                     start_time TIMESTAMP DEFAULT '1970-01-01 00:00:00' NOT NULL,
-                                     end_time TIMESTAMP DEFAULT '1970-01-01 00:00:00' NOT NULL,
+                                     start_time TIMESTAMP DEFAULT 0 NOT NULL,
+                                     end_time TIMESTAMP DEFAULT 0 NOT NULL,
                                      distance SMALLINT NOT NULL DEFAULT 0,
                                      pace VARCHAR(8) NOT NULL,
                                      participant_number SMALLINT NOT NULL,
@@ -45,13 +35,11 @@ CREATE TABLE IF NOT EXISTS tb_bung (
                                      note VARCHAR(4096) DEFAULT '' NULL
 );
 
--- Create table TB_USERS_BUNG if not exists
-CREATE TABLE IF NOT EXISTS tb_users_bung (
+-- tb_users_bung 테이블 생성 (존재하지 않을 경우에만)
+CREATE TABLE IF NOT EXISTS TB_USERS_BUNG (
                                            user_bung_id SERIAL PRIMARY KEY NOT NULL,
-                                           bung_id UUID NOT NULL,
-                                           user_id BIGINT NOT NULL,
-                                           FOREIGN KEY (bung_id) REFERENCES tb_bung(bung_id),
-                                           FOREIGN KEY (user_id) REFERENCES tb_users(user_id),
-                                           CONSTRAINT fk_bung_id FOREIGN KEY (bung_id) REFERENCES tb_bung(bung_id),
-                                           CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES tb_users(user_id)
+                                           bung_id VARCHAR(36) DEFAULT UUID() NOT NULL,
+                                           user_id BIGINT(20) NOT NULL,
+                                           FOREIGN KEY (bung_id) REFERENCES TB_BUNG(bung_id),
+                                           FOREIGN KEY (user_id) REFERENCES TB_USERS(user_id)
 );
