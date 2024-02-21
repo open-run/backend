d3835f382ccede92a80ce0ac3e49a875b793f79a
Son JunHyung <kingjh1125@gmail.com>
2024-02-21 11:19:28 +0900

    BE-13, chore: add global/common config
    common : 공통으로 사용되는 dto 추가
    - response : 응답 dto
    - exception : 예외처리 dto

---
+++ src/main/java/io/openur/global/common/Exception.java
@@ -0,0 +1,10 @@
+package io.openur.global.common;
+
+import org.springframework.http.HttpStatus;
+
+public class Exception {
+    private int statusCode;
+    private HttpStatus state;
+    private String message;
+
+}
