5d0aab7a8ee93de0bb31b478d60af8dafee271bd
Yejin Kelly Joo <yejinkellyjoo@gmail.com>
2024-02-29 16:33:00 +0900

    BE-15, chore: add some comments and modify cicd branch
    Signed-off-by: Yejin Kelly Joo <yejinkellyjoo@gmail.com>

--- src/main/java/io/openur/global/common/Exception.java
+++ src/main/java/io/openur/global/common/Exception.java
@@ -3,7 +3,7 @@ package io.openur.global.common;
 import org.springframework.http.HttpStatus;
 
 public class Exception {
-    private int statusCode;
+    private int statusCode;     // 프론트에서 처리 편의성을 위해
     private HttpStatus state;
     private String message;
 
