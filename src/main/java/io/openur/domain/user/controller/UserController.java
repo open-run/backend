e4ef1a0ee677ad3e93ccacc212cbc5baa7fdabd7
Yejin Kelly Joo <yejinkellyjoo@gmail.com>
2024-03-17 23:12:32 +0900

    BE-15, feature: define `users/login` endpoint controller
    Signed-off-by: Yejin Kelly Joo <yejinkellyjoo@gmail.com>

--- src/main/java/io/openur/domain/user/controller/UserController.java
+++ src/main/java/io/openur/domain/user/controller/UserController.java
@@ -1,9 +1,13 @@
 package io.openur.domain.user.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.openur.domain.user.dto.ExistNicknameRequestDto;
 import io.openur.domain.user.dto.GetUserResponseDto;
-import io.openur.domain.user.dto.PostUserDto;
+import io.openur.domain.user.dto.GetUsersLoginDto;
+import io.openur.domain.user.model.Provider;
 import io.openur.domain.user.service.UserService;
+import io.openur.domain.user.service.oauth.LoginService;
+import io.openur.domain.user.service.oauth.LoginServiceFactory;
 import io.openur.global.common.Response;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -12,29 +16,45 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+    private final LoginServiceFactory loginServiceFactory;
     private final UserService userService;
 
-    @PostMapping("/v1/user")
+    @GetMapping("/v1/users/login/{authServer}")
     @Operation(summary = "유저 로그인 성공 시 정보 반환")
-    public ResponseEntity<Response<PostUserDto>> postUser(){
-        // TODO: load authorized user info from UserService
-        return ResponseEntity.ok()
-            .body(Response.<PostUserDto>builder()
-                .message("success")
-//                .data(new PostUserDto("test"))
-                .build());
+    public ResponseEntity<Response<GetUsersLoginDto>> getUser(
+        @PathVariable Provider authServer,
+        @RequestParam String code,
+        @RequestParam(required = false) String state
+    ){
+        // TODO: different method using Spring Security
+        //  oAuth2AuthorizedClientService.loadAuthorizedClient(authServer.name().toLowerCase(), "test");
+
+        LoginService loginService = loginServiceFactory.getLoginService(authServer);
+		try {
+			return ResponseEntity.ok()
+				.body(Response.<GetUsersLoginDto>builder()
+					.message("success")
+					.data(loginService.login(code, state))
+					.build()
+				);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
     }
 
-    @GetMapping("/v1/users")
+    @GetMapping("/v1/users/{userId}")
     @Operation(summary = "유저 정보 가져오기")
-    public ResponseEntity<Response<GetUserResponseDto>> getUserInfo(@PathVariable Long userId) {
+    public ResponseEntity<Response<GetUserResponseDto>> getUserInfo(
+		// TODO: change to jwtToken as RequestHeader
+		@PathVariable Long userId
+	) {
         GetUserResponseDto getUserResponseDto = userService.getUserById(userId);
         return ResponseEntity.ok().body(Response.<GetUserResponseDto>builder()
             .message("success")
@@ -44,7 +64,9 @@ public class UserController {
 
     @GetMapping("/v1/users/nickname/exist")
     @Operation(summary = "닉네임 중복 체크")
-    public ResponseEntity<Response<Boolean>> existNickname(@RequestBody @Valid ExistNicknameRequestDto existNicknameRequestDto) {
+    public ResponseEntity<Response<Boolean>> existNickname(
+		@RequestBody @Valid ExistNicknameRequestDto existNicknameRequestDto
+	) {
         boolean existNickname = userService.existNickname(
             existNicknameRequestDto.getNickname());
         return ResponseEntity.ok().body(Response.<Boolean>builder()
--- src/main/java/io/openur/global/config/SecurityConfig.java
+++ src/main/java/io/openur/global/config/SecurityConfig.java
@@ -1,61 +1,36 @@
 package io.openur.global.config;
 
-import io.openur.domain.user.service.OAuth2UserService;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final OAuth2UserService oAuthService;
-
-    public SecurityConfig(OAuth2UserService oAuthService) {
-        this.oAuthService = oAuthService;
-    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정
         http.csrf(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-            .anyRequest().authenticated()
+
+        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
+        http.sessionManagement((sessionManagement) ->
+            sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
-        http.oauth2Login(oauth2Configurer -> oauth2Configurer
-            .successHandler(successHandler())
-            .userInfoEndpoint(
-                userInfoEndpoint -> userInfoEndpoint  // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정을 저장
-                    .userService(oAuthService)
-                // OAuth2 로그인 성공 시, 후작업을 진행할 UserService 인터페이스 구현체 등록
-            )
-        );
+
+        // TODO: /v1/users/login 제외 모든 call에 대한 jwt 인증 설정
+//        http.authorizeHttpRequests((authorizeHttpRequests) ->
+//            authorizeHttpRequests
+//                .requestMatchers("/v1/users/**").permitAll() // '/v1/users/'로 시작하는 요청 모두 접근 허가
+//                .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+//        );
+//        http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
+//        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
-    }
-
-    @Bean
-    public AuthenticationSuccessHandler successHandler() {
-        return ((request, response, authentication) -> {
-            DefaultOAuth2User defaultOAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
-
-            String id = defaultOAuth2User.getAttributes().get("id").toString();
-            String body = """
-                {"id":"%s"}
-                """.formatted(id);
-
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-
-            PrintWriter writer = response.getWriter();
-            writer.println(body);
-            writer.flush();
-        });
     }
 }
