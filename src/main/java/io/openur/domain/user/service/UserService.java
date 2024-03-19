ec9f1fc72b3102ce1796cf37813a657d4ef270e4
Yejin Kelly Joo <yejinkellyjoo@gmail.com>
2024-03-19 21:17:39 +0900

    BE-15, refactor: use Optional in UserJpaRepository only
    Signed-off-by: Yejin Kelly Joo <yejinkellyjoo@gmail.com>

--- src/main/java/io/openur/domain/user/service/UserService.java
+++ src/main/java/io/openur/domain/user/service/UserService.java
@@ -3,7 +3,6 @@ package io.openur.domain.user.service;
 import io.openur.domain.user.dto.GetUserResponseDto;
 import io.openur.domain.user.entity.UserEntity;
 import io.openur.domain.user.repository.UserRepositoryImpl;
-import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,8 +14,7 @@ public class UserService {
     private final UserRepositoryImpl userRepository;
 
     public GetUserResponseDto getUserById(Long userId) {
-        UserEntity userEntity = userRepository.findById(userId)
-            .orElseThrow(() -> new NoSuchElementException("User not found"));
+        UserEntity userEntity = userRepository.findById(userId);
         return new GetUserResponseDto(userEntity);
     }
 
