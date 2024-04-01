a45e7ff62dd3045f9d832f4744ca9da7c3278897
jryouda <gh06042001@naver.com>
2024-04-01 13:16:00 +0900

    BE-19, feature : After receiving jwttoken as a header value, modify the function to use it as a parameter.
    BE-19, feature : After receiving jwttoken as a header value, modify the function to use it as a parameter.
    
    BE-19, feature : After receiving jwttoken as a header value, modify the function to use it as a parameter.

--- src/main/java/io/openur/domain/user/service/UserService.java
+++ src/main/java/io/openur/domain/user/service/UserService.java
@@ -1,12 +1,11 @@
 package io.openur.domain.user.service;
-
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
 import io.openur.domain.user.dto.GetUserResponseDto;
 import io.openur.domain.user.dto.PatchUserSurveyRequestDto;
 import io.openur.domain.user.entity.UserEntity;
 import io.openur.domain.user.model.User;
 import io.openur.domain.user.repository.UserRepositoryImpl;
+import io.openur.global.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,30 +15,35 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class UserService {
     private final UserRepositoryImpl userRepository;
+    private final JwtUtil jwtUtil;
 
-    public GetUserResponseDto getUserById(Long userId) {
-        UserEntity userEntity = userRepository.findById(userId);
-        return new GetUserResponseDto(userEntity);
+    public String getUserById(String jwtToken) {
+        Claims claims = jwtUtil.getUserInfoFromToken(jwtToken);
+        // 클레임에서 이메일 추출
+        String email = claims.getSubject();
+        UserEntity userEntity = userRepository.findByEmail(email);
+
+        return userEntity.getUserId();
     }
 
     public boolean existNickname(String nickname) {
         return !userRepository.existsByNickname(nickname);
     }
 
-    public GetUserResponseDto getUserEmail(String authorizationHeader){
-        Claims claims = Jwts.parser().parseClaimsJws(authorizationHeader).getBody();
-
+    public GetUserResponseDto getUserEmail(String jwtToken){
+        Claims claims = jwtUtil.getUserInfoFromToken(jwtToken);
         // 클레임에서 이메일 추출
         String email = claims.getSubject();
-
         UserEntity userEntity = userRepository.findByEmail(email);
         return new GetUserResponseDto(userEntity);
     }
 
     @Transactional
-    public void saveSurveyResult(Long userId, PatchUserSurveyRequestDto patchUserSurveyRequestDto) {
-        User user = User.from(userRepository.findById(userId));
-
+    public void saveSurveyResult(String jwtToken, PatchUserSurveyRequestDto patchUserSurveyRequestDto) {
+        Claims claims = jwtUtil.getUserInfoFromToken(jwtToken);
+        // 클레임에서 이메일 추출
+        String email = claims.getSubject();
+        User user = User.from(userRepository.findByEmail(email));
         user.update(patchUserSurveyRequestDto);
         userRepository.update(user);
     }
