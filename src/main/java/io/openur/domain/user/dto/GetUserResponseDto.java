6d38f35e723fd019edec79c46238e698d1f75f04
Dong Hwan Kim <karmaddee@gmail.com>
2024-06-11 20:35:23 +0900

    BE-33, refactor: formatting date into single date formatted string

--- src/main/java/io/openur/domain/user/dto/GetUserResponseDto.java
+++ src/main/java/io/openur/domain/user/dto/GetUserResponseDto.java
@@ -1,5 +1,6 @@
 package io.openur.domain.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.openur.domain.user.entity.UserEntity;
 import io.openur.domain.user.model.Provider;
 import java.time.LocalDateTime;
@@ -15,7 +16,10 @@ public class GetUserResponseDto {
     private Boolean identityAuthenticated;
     private Provider provider;
     private Boolean blackListed;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime lastLoginDate;
     private String blockchainAddress;
     private String runningPace;
