e8fa89eca437ff737b957561fd6f9ed502007138
Son JunHyung <kingjh1125@gmail.com>
2024-06-18 23:10:51 +0900

    BE-36, feat: add hashtag table and edit users_bungs table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,10 +11,9 @@ CREATE TABLE IF NOT EXISTS tb_users (
                                         blockchain_address VARCHAR(42) NOT NULL DEFAULT '0x',
                                         running_pace VARCHAR(8) DEFAULT NULL,
                                         running_frequency SMALLINT(4) DEFAULT NULL
-
 );
 
--- tb_bung 테이블 생성 (존재하지 않을 경우에만)
+-- tb_bungs 테이블 생성 (존재하지 않을 경우에만)
 CREATE TABLE IF NOT EXISTS tb_bungs (
                                        bung_id VARCHAR(36) DEFAULT (UUID()) PRIMARY KEY NOT NULL,
                                        name VARCHAR(192) NOT NULL DEFAULT '',
@@ -29,11 +28,28 @@ CREATE TABLE IF NOT EXISTS tb_bungs (
                                        after_run_description VARCHAR(4096)
 );
 
--- tb_users_bung 테이블 생성 (존재하지 않을 경우에만)
+-- tb_users_bungs 테이블 생성 (존재하지 않을 경우에만)
 CREATE TABLE IF NOT EXISTS tb_users_bungs (
                                              user_bung_id BIGINT(20) AUTO_INCREMENT PRIMARY KEY NOT NULL,
                                              bung_id VARCHAR(36) DEFAULT (UUID()) NOT NULL,
                                              user_id VARCHAR(36) DEFAULT (UUID()) NOT NULL,
                                              FOREIGN KEY (bung_id) REFERENCES tb_bungs(bung_id),
-                                             FOREIGN KEY (user_id) REFERENCES tb_users(user_id)
+                                             FOREIGN KEY (user_id) REFERENCES tb_users(user_id),
+                                             participation_Status BOOLEAN NOT NULL,
+                                             modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- tb_hashtags 테이블 생성 (존재하지 않을 경우에만)
+CREATE TABLE IF NOT EXISTS tb_hashtags(
+                                            hashtag_id  BIGINT(36) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                                            hashtag_str VARCHAR(36) UNIQUE NOT NULL
+);
+
+-- tb_hashtags 테이블 생성 (존재하지 않을 경우에만)
+CREATE TABLE IF NOT EXISTS tb_bungs_hashtags (
+                                              bung_hashtag_id BIGINT(20) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                                              bung_id VARCHAR(36) DEFAULT (UUID()) NOT NULL,
+                                              hashtag_id BIGINT(36) NOT NULL,
+                                              FOREIGN KEY (bung_id) REFERENCES tb_bungs(bung_id),
+                                              FOREIGN KEY (hashtag_id) REFERENCES tb_hashtags(hashtag_id)
 );
