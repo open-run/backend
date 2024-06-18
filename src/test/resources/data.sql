684f69f17922b6384ce4ff577b971dd162c95f82
Yejin Kelly Joo <yejinkellyjoo@gmail.com>
2024-06-18 21:20:01 +0900

    BE-35, fix:  remove remaining `withdraw` related values
    Signed-off-by: Yejin Kelly Joo <yejinkellyjoo@gmail.com>

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,9 +1,13 @@
 -- h2 data sql sheet for test implementation, ** must be executed in order of foreign key constraints **
-INSERT INTO tb_users (user_id, withdraw, nickname, email, identity_authenticated, provider, blacklisted, created_date, last_login_date, blockchain_address, running_pace, running_frequency)
-VALUES
-('1', FALSE, 'test', 'test1@test.com', FALSE, 'naver', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '0x', NULL, NULL),
-('2', FALSE, 'test2', 'test2@test.com', FALSE, 'naver', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '0x', NULL, NULL),
-('3', FALSE, 'test3', 'test3@test.com', FALSE, 'naver', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '0x', NULL, NULL)
+INSERT INTO tb_users (user_id, nickname, email, identity_authenticated, provider, blacklisted,
+                      created_date, last_login_date, blockchain_address, running_pace,
+                      running_frequency)
+VALUES ('1', 'test', 'test1@test.com', FALSE, 'naver', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,
+        '0x', NULL, NULL),
+       ('2', 'test2', 'test2@test.com', FALSE, 'naver', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,
+        '0x', NULL, NULL),
+       ('3', 'test3', 'test3@test.com', FALSE, 'naver', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,
+        '0x', NULL, NULL)
 ;
 
 INSERT INTO tb_bung (bung_id, location, datetime, bung_name, start_time, end_time, distance, pace, participant_number, has_after_run, note)
