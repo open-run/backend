94002193f552cc7a193543374b918828e5abec0b
Dong Hwan Kim <karmaddee@gmail.com>
2024-06-24 17:45:20 +0900

    BE-37, feature: api for get bung detail

--- src/main/java/io/openur/domain/bung/controller/BungController.java
+++ src/main/java/io/openur/domain/bung/controller/BungController.java
@@ -7,11 +7,16 @@ import io.openur.global.common.Response;
 import io.openur.global.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,6 +38,29 @@ public class BungController {
         return ResponseEntity.ok().body(Response.<GetBungDetailDto>builder()
             .message("success")
             .data(createBung)
+            .build());
+    }
+
+    @GetMapping()
+    @Operation(summary = "벙 목록을 보는 경우 || 전체보기 || 참여한 ||")
+    public ResponseEntity<Response> getBungList(
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @RequestParam(required = false, defaultValue = "false") boolean isOwned,
+        @PageableDefault Pageable pageable
+    ) {
+        return null;
+    } // TODO: users_bungs 가 생기면, users_bungs type 기준으로 필터 및 join 탐색 시행, boolean 교체
+
+    @GetMapping("/{bungId}")
+    @Operation(summary = "벙 정보 상세보기")
+    public ResponseEntity<Response<GetBungDetailDto>> getBungDetail(
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @PathVariable String bungId
+    ) {
+        GetBungDetailDto getBung = bungService.getBungDetail(userDetails, bungId);
+        return ResponseEntity.ok().body(Response.<GetBungDetailDto>builder()
+            .message("success")
+            .data(getBung)
             .build());
     }
 }
