e82375443aafc0ca774b4126272bfcf3c48662a4
jryouda <gh06042001@naver.com>
2024-07-22 09:06:02 +0900

    BE-44 test : Check if data in db has been changed properly. Add status not ok check(401 unauthorized, 403 forbidden)

--- src/test/java/io/openur/controller/BungControllerTest.java
+++ src/test/java/io/openur/controller/BungControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 public class BungControllerTest extends TestSupport {
     private static final String PREFIX = "/v1/bungs";
 
+
     @Test
     @DisplayName("Bung : 벙 생성 테스트")
     void createBungTest() throws Exception {
@@ -39,18 +40,59 @@ public class BungControllerTest extends TestSupport {
         ).andExpect(status().isCreated());
     }
 
-    @Test
-    @DisplayName("Bung : 벙주 변경 테스트")
-    void changeOwnerTest() throws Exception {
-        String token = getTestUserToken();
+    @Nested
+    class changeOwnerTest {
+        @Test
+        @DisplayName("Bung : 벙주 변경 테스트")
+        void changeOwner_isOkTest() throws Exception {
+            String token = getTestUserToken();
 
-        String bungId = "벙Id";
-        String newOwnerUserId = "뉴네오딮벙주";
+            String bungId = "c0477004-1632-455f-acc9-04584b55921f";
+            String newOwnerUserId = "91b4928f-8288-44dc-a04d-640911f0b2be";
+            String oldOwnerUserId = "9e1bfc60-f76a-47dc-9147-803653707192";
 
-        mockMvc.perform(
-            patch(PREFIX + "/{bungId}/change-owner?newOwnerUserId={newOwnerUserId}", bungId, newOwnerUserId)
-                .header(AUTH_HEADER, token)
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andExpect(status().isOk());
+            mockMvc.perform(
+                patch(PREFIX + "/{bungId}/change-owner?newOwnerUserId={newOwnerUserId}", bungId, newOwnerUserId)
+                    .header(AUTH_HEADER, token)
+                    .contentType(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isOk());
+
+            Optional<UserBungEntity> newOwnerBung = userBungJpaRepository
+                .findByUserEntity_UserIdAndBungEntity_BungId(newOwnerUserId, bungId);
+            assertThat(newOwnerBung).isPresent();
+            assertThat(newOwnerBung.get().isOwner()).isTrue();
+
+            Optional<UserBungEntity> oldOwnerBung = userBungJpaRepository
+                .findByUserEntity_UserIdAndBungEntity_BungId(oldOwnerUserId, bungId);
+            assertThat(oldOwnerBung).isPresent();
+            assertThat(oldOwnerBung.get().isOwner()).isFalse();
+        }
+
+        @Test
+        @DisplayName("벙주 변경 실패 - Authorization Header 없음. 403 Forbidden")
+        void changeOwner_forbiddenTest() throws Exception {
+            String bungId = "c0477004-1632-455f-acc9-04584b55921f";
+            String newOwnerUserId = "91b4928f-8288-44dc-a04d-640911f0b2be";
+
+            mockMvc.perform(
+                patch(PREFIX + "/{bungId}/change-owner?newOwnerUserId={newOwnerUserId}", bungId, newOwnerUserId)
+                    .contentType(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isForbidden());
+        }
+
+        @Test
+        @DisplayName("벙주 변경 실패 - 잘못된 Authorization Header 401 Unauthorized")
+        void changeOwner_unauthorizedTest() throws Exception {
+            String invalidToken = "invalidToken";
+
+            String bungId = "c0477004-1632-455f-acc9-04584b55921f";
+            String newOwnerUserId = "91b4928f-8288-44dc-a04d-640911f0b2be";
+
+            mockMvc.perform(
+                patch(PREFIX + "/{bungId}/change-owner?newOwnerUserId={newOwnerUserId}", bungId, newOwnerUserId)
+                    .header(AUTH_HEADER, invalidToken)
+                    .contentType(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isUnauthorized());
+        }
     }
 }
