1065f5fce73c47086fe8ff51dfbcc44b3bd06261
Dong Hwan Kim <karmaddee@gmail.com>
2024-09-01 02:13:24 +0900

    BE-46, fix: owned bung list pagination and logic

--- src/main/java/io/openur/domain/bung/repository/BungRepositoryImpl.java
+++ src/main/java/io/openur/domain/bung/repository/BungRepositoryImpl.java
@@ -11,12 +11,10 @@ import io.openur.domain.bung.dto.BungDetailDto;
 import io.openur.domain.bung.entity.BungEntity;
 import io.openur.domain.bung.model.Bung;
 import io.openur.domain.bung.repository.dao.BungDAO;
-import io.openur.domain.user.model.User;
 import io.openur.global.enums.BungStatus;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -50,13 +48,13 @@ public class BungRepositoryImpl implements BungRepository, BungDAO {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Page<BungDetailDto> findBungs(User user, BungStatus status, Pageable pageable) {
+    public Page<BungDetailDto> findBungs(String userId, BungStatus status, Pageable pageable) {
         List<BungDetailDto> contents = queryFactory
-            .selectDistinct(userBungEntity.bungEntity)
-            .from(userBungEntity)
+            .selectDistinct(bungEntity)
+            .from(bungEntity, userBungEntity)
             .join(userBungEntity.bungEntity, bungEntity)
             .where(
-                statusFilter(user, status)
+                statusFilter(userId, status)
             )
             .offset(pageable.getOffset())
             .limit(pageable.getPageSize())
@@ -64,17 +62,51 @@ public class BungRepositoryImpl implements BungRepository, BungDAO {
             .fetch().stream().map(entity -> new BungDetailDto(Bung.from(entity))).toList();
 
         JPAQuery<Long> count = queryFactory
-            .selectDistinct(userBungEntity.bungEntity.count())
-            .from(userBungEntity)
+            .selectDistinct(bungEntity.countDistinct())
+            .from(bungEntity, userBungEntity)
             .join(userBungEntity.bungEntity, bungEntity)
             .where(
-                statusFilter(user, status)
+                statusFilter(userId, status)
             );
 
         return PageableExecutionUtils.getPage(contents, pageable, count::fetchOne);
     }
 
-    private BooleanExpression statusFilter(User user, BungStatus status) {
+    @Override
+    public Page<BungDetailDto> findOwnedBungs(String userId, Pageable pageable) {
+        List<BungDetailDto> contents = queryFactory
+            .selectDistinct(userBungEntity.bungEntity)
+            .from(userBungEntity)
+            .join(userBungEntity.bungEntity, bungEntity)
+            .join(userBungEntity.userEntity, userEntity)
+            .where(
+                userBungEntity.isOwner.isTrue(),
+                userBungEntity.userEntity.userId.eq(userId)
+            )
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .orderBy(bungEntity.startDateTime.desc())
+            .fetch().stream().map(entity -> new BungDetailDto(Bung.from(entity))).toList();
+
+        JPAQuery<Long> count = queryFactory
+            .selectDistinct(userBungEntity.bungEntity.countDistinct())
+            .from(userBungEntity)
+            .join(userBungEntity.bungEntity, bungEntity)
+            .join(userBungEntity.userEntity, userEntity)
+            .where(
+                userBungEntity.isOwner.isTrue(),
+                userBungEntity.userEntity.userId.eq(userId)
+            );
+
+        return PageableExecutionUtils.getPage(contents, pageable, count::fetchOne);
+//        List<UserBungEntity> userBungEntities = userBungJpaRepository.findByUserEntity_UserIdAndIsOwnerTrue(userId);
+//        return userBungEntities.stream()
+//            .map(UserBungEntity::getBungEntity)
+//            .map(Bung::from)
+//            .collect(Collectors.toList());
+    }
+
+    private BooleanExpression statusFilter(String userId, BungStatus status) {
         LocalDateTime currentTime = LocalDateTime.now();
 
         return switch (status) {
@@ -82,9 +114,9 @@ public class BungRepositoryImpl implements BungRepository, BungDAO {
             case FINISHED -> bungEntity.startDateTime.gt(currentTime).and(bungEntity.endDateTime.gt(currentTime));
             //TODO: ** 수정 필요합니다. 참여가 가능한 벙, 시작 마감 이전이며, 참여 혹은 소유 하지 않은 벙 ( userBung 에 어떻게든 존재할 방법은 소유주 혹은 참가자 )
             case AVAILABLE -> bungEntity.startDateTime.lt(currentTime)
-                .and(userBungEntity.isOwner.isFalse().or(userBungEntity.userEntity.ne(user.toEntity())));
+                .and(userBungEntity.isOwner.isFalse().or(userBungEntity.userEntity.userId.ne(userId)));
             // 이미 참여한 벙 = 내가 주인이 아니면서, 참여 이력이 존재한 user bung entity
-            case JOINED -> userEntity.eq(user.toEntity()).and(userBungEntity.isOwner.isFalse());
+            case JOINED -> userEntity.userId.ne(userId).and(userBungEntity.isOwner.isFalse());
             // All 인 케이스, null 이더라도 포함이 되는 케이스
             default -> bungEntity.startDateTime.lt(currentTime);
         };
