9e2093d87f0158973e46d43a95c4b74664389e63
Yejin Kelly Joo <yejinkellyjoo@gmail.com>
2024-08-31 18:53:10 +0900

    refactor: createAccounts -> createAccount
    Signed-off-by: Yejin Kelly Joo <yejinkellyjoo@gmail.com>

--- src/test/java/io/openur/service/XrplTest.java
+++ src/test/java/io/openur/service/XrplTest.java
@@ -1,7 +1,6 @@
 package io.openur.service;
 
 import io.openur.domain.xrpl.repository.XrplRepository;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,27 +15,29 @@ public class XrplTest {
 	XrplRepository xrplRepository;
 
 	@Test
-	void testCreateAccounts() throws InterruptedException {
-		List<KeyPair> accounts = xrplRepository.createAccounts();
+	void testCreateAccounts() throws InterruptedException, JsonRpcClientErrorException {
+		KeyPair account = xrplRepository.createAccount();
 
-		for (KeyPair account : accounts) {
-			try {
-				System.out.println("wallet address: " + account.publicKey().deriveAddress());
+		System.out.println("wallet address: " + account.publicKey().deriveAddress());
 
-				// accountInfo 메서드를 사용하여 계정 정보를 조회합니다.
-				AccountInfoResult result = xrplRepository.accountInfo(account);
+		// accountInfo 메서드를 사용하여 계정 정보를 조회합니다.
+		AccountInfoResult result = xrplRepository.accountInfo(account);
 
-				// 조회된 계정 정보 출력
-				System.out.println("Account Balance: " + result.accountData().balance());
-				System.out.println("Sequence: " + result.accountData().sequence());
-				System.out.println("Owner Count: " + result.accountData().ownerCount());
-				System.out.println("account: " + result.accountData().account());
-				System.out.println();
-			} catch (JsonRpcClientErrorException e) {
-				// JSON-RPC 오류 처리
-				System.err.println("Failed to retrieve account info: " + e.getMessage());
-			}
-		}
+		// 조회된 계정 정보 출력
+		System.out.println("Account Balance: " + result.accountData().balance());
+		System.out.println("Sequence: " + result.accountData().sequence());
+		System.out.println("Owner Count: " + result.accountData().ownerCount());
+		System.out.println("account: " + result.accountData().account());
+		System.out.println();
+	}
+
+	@Test
+	void testMint() throws InterruptedException, JsonRpcClientErrorException {
+		KeyPair keyPair = xrplRepository.createAccount();
+		KeyPair minterKeyPair = xrplRepository.createAccount();
+
+		xrplRepository.mintFromOtherMinterAccount(keyPair, minterKeyPair);
+
 	}
 
 }
