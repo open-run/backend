aa9dc4d2d220092bae03d2c3e10d4f7d04e9eadb
yjkellyjoo <yejinkellyjoo@gmail.com>
2024-10-21 10:55:26 +0900

    BE-53, feat: Get Frequent User Suggestion
    Signed-off-by: yjkellyjoo <yejinkellyjoo@gmail.com>

--- src/main/java/io/openur/domain/userbung/repository/UserBungDAO.java
+++ src/main/java/io/openur/domain/userbung/repository/UserBungDAO.java
@@ -1,13 +1,17 @@
 package io.openur.domain.userbung.repository;
 
+import com.querydsl.core.Tuple;
 import io.openur.domain.bung.dto.BungDetailDto;
 import io.openur.domain.bung.model.Bung;
 import io.openur.domain.bung.model.BungStatus;
 import io.openur.domain.user.model.User;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface UserBungDAO {
     BungDetailDto findJoinedUsersByBungId(String bungId);
     Page<Bung> findJoinedBungsByUserWithStatus(User user, BungStatus status, Pageable pageable);
+    List<String> findJoinedBungsId(User user);
+    Page<Tuple> findAllFrequentUsers(List<String> bungIds, User user, Pageable pageable);
 }
