aba57164c9c968ca3b1d7c42ea32fee89205a1c7
Yejin Kelly Joo <yejinkellyjoo@gmail.com>
2024-10-22 18:26:42 +0900

    BE-55, test: add sample data in data.sql and test code for GET /v1/bungs/hashtags
    Signed-off-by: Yejin Kelly Joo <yejinkellyjoo@gmail.com>

--- src/main/java/io/openur/global/common/Response.java
+++ src/main/java/io/openur/global/common/Response.java
@@ -1,10 +1,14 @@
 package io.openur.global.common;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Response<T> {
     private String message;
     private T data;
