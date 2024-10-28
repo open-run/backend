dd90e108932d91e8dfd708748cb41f7545b4c254
yjkellyjoo <yejinkellyjoo@gmail.com>
2024-10-28 20:33:58 +0900

    BE-54, refactor: @sonjh919 comment, .collect(Collectors.toList()) -> .toList()
    Signed-off-by: yjkellyjoo <yejinkellyjoo@gmail.com>

--- src/main/java/io/openur/domain/bunghashtag/repository/BungHashtagRepositoryImpl.java
+++ src/main/java/io/openur/domain/bunghashtag/repository/BungHashtagRepositoryImpl.java
@@ -4,7 +4,6 @@ import io.openur.domain.bung.model.Bung;
 import io.openur.domain.bunghashtag.model.BungHashtag;
 import io.openur.domain.hashtag.model.Hashtag;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -21,20 +20,19 @@ public class BungHashtagRepositoryImpl implements BungHashtagRepository {
     @Override
     public List<Hashtag> findHashtagsByBungId(String bungId) {
         return bungHashtagJpaRepository.findByBungEntity_BungId(bungId).stream()
-                .map(Hashtag::from)
-                .collect(Collectors.toList());
+                .map(Hashtag::from).toList();
     }
 
     @Override
     public List<Bung> findBungByHashtag(String hashtagStr) {
         return bungHashtagJpaRepository.findBungEntityByHashtagEntity_HashtagStr(hashtagStr)
-                .stream().map(Bung::from).collect(Collectors.toList());
+                .stream().map(Bung::from).toList();
     }
 
     @Override
     public List<Bung> findBungByHashtags(List<String> hashtagStrs) {
         return bungHashtagJpaRepository.findBungEntityByHashtagEntity_HashtagStrIn(hashtagStrs)
-                .stream().map(Bung::from).collect(Collectors.toList());
+                .stream().map(Bung::from).toList();
     }
 
     @Override
