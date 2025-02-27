33c3ffcb4f13a63cd3306a3deef4693d1994155a
Dong Hwan Kim <karmaddee@gmail.com>
2025-02-27 20:43:45 +0900

    BE-80, test: fix data for detailed testing ( ex : orders )

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -18,11 +18,11 @@ INSERT INTO tb_bungs (bung_id, name, location, description, start_datetime, end_
 VALUES ('c0477004-1632-455f-acc9-04584b55921f', 'test1_bung', 'Seoul', 'temp_description',
         CURRENT_TIMESTAMP + 1, CURRENT_TIMESTAMP + 2, 3, '5"55', 2, false, null, false, 'image1.png'),
        ('90477004-1422-4551-acce-04584b34612e', 'test2_bung', 'Bangkok', 'second_bung_data',
-        CURRENT_TIMESTAMP + 1, CURRENT_TIMESTAMP + 2, 1, '6"30', 2, true, 'chicken & beer', false, 'image2.png'),
+        CURRENT_TIMESTAMP + 3, CURRENT_TIMESTAMP + 4, 1, '6"30', 2, true, 'chicken & beer', false, 'image2.png'),
        ('a1234567-89ab-cdef-0123-456789abcdef', 'past_bung_incompleted', 'New York', 'past_bung_description',
         CURRENT_TIMESTAMP - 2, CURRENT_TIMESTAMP - 1, 5, '7"00', 3, false, null, false, 'image3.png'),
        ('a1234567-89ab-cdef-0123-1982ey1kbjas', 'past_bung_completed', 'New York', 'past_bung_description',
-        CURRENT_TIMESTAMP - 2, CURRENT_TIMESTAMP - 1, 5, '7"00', 3, false, null, true, 'image4.png')
+        CURRENT_TIMESTAMP - 4, CURRENT_TIMESTAMP - 3, 5, '7"00', 3, false, null, true, 'image4.png')
 ;
 
 INSERT INTO tb_users_bungs (user_bung_id, bung_id, user_id, participation_status, modified_at,
