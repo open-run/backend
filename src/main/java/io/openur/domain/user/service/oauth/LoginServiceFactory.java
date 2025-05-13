adb24f4d18bfb9069633e38b6ffdec571829b293
Yejin Kelly Joo <yejinkellyjoo@gmail.com>
2025-05-13 18:45:32 +0900

    BE-91 Feature Implement Smart Wallet user login features
    - Implemented user registration logic in SmartWalletService to handle new user creation with smart wallet login.
    - Introduced SmartWalletUserInfoDto for handling user information related to smart wallet user.
    - Added a User model constructor to create users with blockchain address.
    - Added repository methods to find users by blockchain address in UserJpaRepository and UserRepository.
    - Updated LoginServiceFactory to include SmartWalletService for provider management.
    
    Signed-off-by: yjkellyjoo <yejinkellyjoo@gmail.com>

--- src/main/java/io/openur/domain/user/service/oauth/LoginServiceFactory.java
+++ src/main/java/io/openur/domain/user/service/oauth/LoginServiceFactory.java
@@ -10,12 +10,14 @@ public class LoginServiceFactory {
 
     private final KakaoService kakaoService;
     private final NaverService naverService;
+    private final SmartWalletService smartWalletService;
 
     public LoginService getLoginService(Provider provider) {
         // TODO: login challenge publisher
         return switch (provider) {
             case kakao -> kakaoService;
             case naver -> naverService;
+            case smart_wallet -> smartWalletService;
             default -> throw new IllegalArgumentException("Invalid provider");
         };
     }
