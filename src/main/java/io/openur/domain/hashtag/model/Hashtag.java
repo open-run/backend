9b49277b2129d5d74179333cce486c92fe50304a
Dong Hwan Kim <karmaddee@gmail.com>
2025-05-13 15:01:49 +0900

    BE-92, fix : Bung, HashTag, BungHashtag structure revision
    - Remove the use of ManyToMany - Join Table

--- src/main/java/io/openur/domain/hashtag/model/Hashtag.java
+++ src/main/java/io/openur/domain/hashtag/model/Hashtag.java
@@ -1,31 +1,48 @@
 package io.openur.domain.hashtag.model;
 
 import io.openur.domain.bunghashtag.entity.BungHashtagEntity;
+import io.openur.domain.bunghashtag.model.BungHashtag;
 import io.openur.domain.hashtag.entity.HashtagEntity;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
 @AllArgsConstructor
 public class Hashtag {
-
+    @Getter
     private Long hashtagId;
+    @Getter
     private String hashtagStr;
+    
+    private List<BungHashtagEntity> bungHashtags;
 
     public Hashtag(String hashtagStr) {
         this.hashtagStr = hashtagStr;
     }
 
-    public static Hashtag from(final BungHashtagEntity bungHashtagEntity) {
-        return new Hashtag(bungHashtagEntity.getHashtagEntity().getHashtagId(),
-            bungHashtagEntity.getHashtagEntity().getHashtagStr());
-    }
+//    public static Hashtag from(final BungHashtagEntity bungHashtagEntity) {
+//        return new Hashtag(
+//            bungHashtagEntity.getHashtagEntity().getHashtagId(),
+//            bungHashtagEntity.getHashtagEntity().getHashtagStr(),
+//
+//        );
+//    }
 
     public static Hashtag from(final HashtagEntity hashtagEntity) {
-        return new Hashtag(hashtagEntity.getHashtagId(), hashtagEntity.getHashtagStr());
+        return new Hashtag(
+            hashtagEntity.getHashtagId(),
+            hashtagEntity.getHashtagStr(),
+            hashtagEntity.getBungHashtags()
+        );
     }
 
     public HashtagEntity toEntity() {
-        return new HashtagEntity(this.hashtagId, this.hashtagStr);
+        return new HashtagEntity(
+            this.hashtagId, this.hashtagStr, this.bungHashtags
+        );
+    }
+    
+    private List<BungHashtagEntity> getBungHashtags() {
+        return bungHashtags;
     }
 }
