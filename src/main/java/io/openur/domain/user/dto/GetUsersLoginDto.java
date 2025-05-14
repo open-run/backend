d57c0b1b31e703eec7980d2065deeb2a10c92af2
Yejin Kelly Joo <yejinkellyjoo@gmail.com>
2025-05-14 17:55:10 +0900

    BE-91 Fix revive `email` field in `GetUsersLoginDto`
    for backward compatibility
    
    Signed-off-by: Yejin Kelly Joo <yejinkellyjoo@gmail.com>

--- src/main/java/io/openur/domain/user/dto/GetUsersLoginDto.java
+++ src/main/java/io/openur/domain/user/dto/GetUsersLoginDto.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class GetUsersLoginDto {
-
+    @Deprecated
+    private final String email;       // TODO: 삭제 예정
     private final String identifier;  // Can be email or blockchain address
     private final String nickname;
     private final String jwtToken;
