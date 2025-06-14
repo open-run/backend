87756ab8ce0e0156d86f9321c0cb7cd584ad23de
Dong Hwan Kim <karmaddee@gmail.com>
2025-06-14 21:57:31 +0900

    BE-00, fix : missing enum type designation
    - add missing basic findById implementation

--- src/main/java/io/openur/domain/challenge/entity/ChallengeEntity.java
+++ src/main/java/io/openur/domain/challenge/entity/ChallengeEntity.java
@@ -5,6 +5,8 @@ import io.openur.domain.challenge.model.CompletedType;
 import io.openur.domain.challenge.model.RewardType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,16 +27,19 @@ public class ChallengeEntity {
     
     private String name;
     private String description;
-    
+
+    @Enumerated(EnumType.STRING)
     @Column(name = "challenge_type")
     private ChallengeType challengeType;
-    
+
+    @Enumerated(EnumType.STRING)
     @Column(name = "reward_type")
     private RewardType rewardType;
     
     @Column(name = "reward_percentage")
     private Float rewardPercentage;
-    
+
+    @Enumerated(EnumType.STRING)
     @Column(name = "completed_type")
     private CompletedType completedType;
     
