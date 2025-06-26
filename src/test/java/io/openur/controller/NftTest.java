ccaa4d2660d061ac53a54f5af21c980b23a8f264
jryouda <gh06042001@naver.com>
2025-06-26 17:04:18 +0900

    BE-93, NFT 이미지 민팅#2(매핑 필요함)

--- src/test/java/io/openur/controller/NftTest.java
+++ src/test/java/io/openur/controller/NftTest.java
@@ -23,16 +23,16 @@ public class NftTest {
         OpenRunNFTTest contract = OpenRunNFTTest.load(contractAddress, web3j, credentials, new DefaultGasProvider());
 
         String to = "0x4f76b5121838b16a5f938d4c8369021f22ad659d";
-        BigInteger taskId = BigInteger.valueOf(2);
+        BigInteger taskId = BigInteger.valueOf(403);
         BigInteger amount = BigInteger.ONE;
-        BigInteger itemId = BigInteger.valueOf(2);
+        BigInteger itemId = BigInteger.valueOf(403);
 
         // task 등록
         contract.setTaskItem(taskId, itemId).send();
         System.out.println("taskId " + taskId + "에 itemId " + itemId + " 등록 완료");
 
 
-        String baseUri = "ipfs://bafkreihs36dg2d4jfie2igmnvhfgmw26szduesb6bun7mgp7ffxcuj6eee";
+        String baseUri = "ipfs://bafybeigi2w2q6zeq66uk27wk7uwcwkli6fbwc73lsezxpets7x43r2s6o4/";
 
         // baseURI 설정
         TransactionReceipt uriReceipt = contract.setBaseURI(baseUri).send();
@@ -44,5 +44,34 @@ public class NftTest {
         // NFT 민팅
         TransactionReceipt receipt = contract.mintItemForTask(to, taskId, amount).send();
         System.out.println("Mint Success! TxHash: " + receipt.getTransactionHash());
+
+
+
+        // 확인할 지갑 주소와 tokenId
+        String walletAddress = "0x4f76b5121838b16a5f938d4c8369021f22ad659d";
+        BigInteger tokenId = BigInteger.valueOf(403);
+
+        //보유량 확인
+        BigInteger balance = contract.balanceOf(walletAddress, tokenId).send();
+        System.out.println("NFT 보유량 (tokenId = " + tokenId + "): " + balance);
+
+        if (balance.compareTo(BigInteger.ZERO) > 0) {
+            //메타데이터 URI 조회
+            String uri = contract.uri(tokenId).send();
+            System.out.println("메타데이터 URI: " + uri);
+
+            //tokenId 403에 대해 메타데이터 파일명 치환
+            if (tokenId.equals(BigInteger.valueOf(403))) {
+                uri = uri.replace("403", "shoes4-3.json");
+            }
+
+            //게이트웨이 URL로 변환
+            String metadataUrl = uri.replace("ipfs://", "https://gateway.pinata.cloud/ipfs/");
+            System.out.println("메타데이터 확인: " + metadataUrl);
+
+            System.out.println("\n 브라우저에서 확인:\n" + metadataUrl);
+        } else {
+            System.out.println(" 해당 지갑은 이 NFT를 보유하고 있지 않습니다.");
+        }
     }
 }
