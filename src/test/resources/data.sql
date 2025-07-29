8fa93604a42ba594beacd2e5b82174287ae242ae
Yejin Kelly Joo <yejinkellyjoo@gmail.com>
2025-07-29 16:46:51 +0200

    BE-93 Refactor: Update test user token methods
     to use blockchain addresses instead of emails in test cases
    
    Signed-off-by: Yejin Kelly Joo <yejinkellyjoo@gmail.com>

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,16 +1,16 @@
 -- h2 data sql sheet for test implementation, ** must be executed in order of foreign key constraints **
-INSERT INTO tb_users (user_id, nickname, email, identity_authenticated, provider, blacklisted,
+INSERT INTO tb_users (user_id, nickname, identity_authenticated, provider, blacklisted,
                       created_date, last_login_date, blockchain_address, running_pace,
                       running_frequency)
-VALUES ('9e1bfc60-f76a-47dc-9147-803653707192', 'test', 'test1@test.com', FALSE, 'naver', FALSE, CURRENT_TIMESTAMP,
+VALUES ('9e1bfc60-f76a-47dc-9147-803653707192', 'test', FALSE, 'smart_wallet', FALSE, CURRENT_TIMESTAMP,
         CURRENT_TIMESTAMP,
-        '0x', NULL, NULL),
-       ('91b4928f-8288-44dc-a04d-640911f0b2be', 'test2', 'test2@test.com', FALSE, 'naver', FALSE, CURRENT_TIMESTAMP,
+        '0x1234567890123456789012345678901234567890', NULL, NULL),
+       ('91b4928f-8288-44dc-a04d-640911f0b2be', 'test2', FALSE, 'smart_wallet', FALSE, CURRENT_TIMESTAMP,
         CURRENT_TIMESTAMP,
-        '0x', NULL, NULL),
-       ('5d22bd65-f1ed-4e7b-bc7b-0a59580d3176', 'test3', 'test3@test.com', FALSE, 'naver', FALSE, CURRENT_TIMESTAMP,
+        '0x1234567890123456789012345678901234567891', NULL, NULL),
+       ('5d22bd65-f1ed-4e7b-bc7b-0a59580d3176', 'test3', FALSE, 'smart_wallet', FALSE, CURRENT_TIMESTAMP,
         CURRENT_TIMESTAMP,
-        '0x', NULL, NULL)
+        '0x1234567890123456789012345678901234567892', NULL, NULL)
 ;
 
 INSERT INTO tb_bungs (bung_id, name, location, description, start_datetime, end_datetime, distance, pace, member_number,
