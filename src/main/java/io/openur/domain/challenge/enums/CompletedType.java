90aa25509dc3456cf1e24f68577a78e8a85d302f
Dong Hwan Kim <karmaddee@gmail.com>
2025-10-14 00:00:46 +0900

    BE-87, feat : new Challenge Staging Entity for Repetitives

--- src/main/java/io/openur/domain/challenge/enums/CompletedType.java
+++ src/main/java/io/openur/domain/challenge/enums/CompletedType.java
@@ -1,5 +1,15 @@
 package io.openur.domain.challenge.enums;
 
+// 기본적으로 모든 도전과제는 횟수 식이다, 최소 한번 부터 시작하니까.
 public enum CompletedType {
-    date, count, place, wearing, pace
+    // 날짜를 입력 받자
+    date,
+    // 장소 주소..?
+    place,
+    // 착용한 옷
+    wearing,
+    // 달성 페이스
+    pace,
+    
+//    count || 모든 도전과제가 기본적으로 횟수라는 제한을 가지고, 떄문에 기본적인 사항일뿐, 따로 여길 필요는 없다.
 }
