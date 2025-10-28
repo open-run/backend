5bf67a7458ffe7ef00df64eebe6ef5881ebc1608
Dong Hwan Kim <karmaddee@gmail.com>
2025-10-28 22:18:45 +0900

    refactor : challenge stage entities are now in

--- src/main/java/io/openur/domain/challenge/dto/GeneralChallengeDto.java
+++ src/main/java/io/openur/domain/challenge/dto/GeneralChallengeDto.java
@@ -1,8 +1,9 @@
 package io.openur.domain.challenge.dto;
 
-import io.openur.domain.challenge.model.Challenge;
 import io.openur.domain.challenge.enums.ChallengeType;
 import io.openur.domain.challenge.enums.CompletedType;
+import io.openur.domain.challenge.model.Challenge;
+import io.openur.domain.challenge.model.ChallengeStage;
 import io.openur.domain.userchallenge.model.UserChallenge;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,6 +15,7 @@ public class GeneralChallengeDto {
     private Long challengeId;
     private Long userChallengeId;
     private String challengeName;
+    private LocalDateTime completedDate;
     private String challengeDescription;
     private Integer currentCount;
     private Integer conditionCount;
@@ -22,25 +24,31 @@ public class GeneralChallengeDto {
     private ChallengeType challengeType;
     private CompletedType completedType;
     private float progressStat = 0.0f;
-    private boolean completed;
+    private Integer stageCount;
+    private boolean accomplished;
     private boolean nftCompleted;
 
     public GeneralChallengeDto(UserChallenge userChallenge) {
-        Challenge challenge = userChallenge.getChallengeStage();
-
-        this.challengeId = challenge.getChallengeId();
         this.userChallengeId = userChallenge.getUserChallengeId();
+        this.currentCount = userChallenge.getCurrentCount();
+        this.completedDate = userChallenge.getCompletedDate();
+        this.accomplished = userChallenge.getCompletedDate() != null;
+        this.nftCompleted = userChallenge.getNftCompleted();
+
+        ChallengeStage challengeStage = userChallenge.getChallengeStage();
+        this.conditionCount = challengeStage.getConditionAsCount();
+        this.stageCount = challengeStage.getStageNumber();
+
+        Challenge challenge = challengeStage.getChallenge();
+        this.challengeId = challenge.getChallengeId();
         this.challengeName = challenge.getChallengeName();
         this.challengeDescription = challenge.getChallengeDescription();
-        this.currentCount = userChallenge.getCurrentCount();
 //        this.conditionCount = challenge.getCompletedConditionCount();
+        this.completedType = challenge.getCompletedType();
         this.conditionDate = challenge.getCompletedConditionDate();
         this.conditionText = challenge.getCompletedConditionText();
         this.challengeType = challenge.getChallengeType();
-        this.completedType = challenge.getCompletedType();
         this.progressStat = ((float) this.currentCount) / ((float)  this.conditionCount) * 100;
-        this.completed = userChallenge.getCompletedDate() != null;
-        this.nftCompleted = userChallenge.getNftCompleted();
     }
 
     @Getter
