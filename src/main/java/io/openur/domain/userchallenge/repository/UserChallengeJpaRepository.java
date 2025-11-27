ab08f8b5d594dae794789874d9da78b2a2b911a7
Dong Hwan Kim <karmaddee@gmail.com>
2025-11-27 21:22:43 +0900

    BE-87, fix : queryDsl fetch join and hierarchical errors

--- src/main/java/io/openur/domain/userchallenge/repository/UserChallengeJpaRepository.java
+++ src/main/java/io/openur/domain/userchallenge/repository/UserChallengeJpaRepository.java
@@ -1,25 +1,8 @@
 package io.openur.domain.userchallenge.repository;
 
 import io.openur.domain.userchallenge.entity.UserChallengeEntity;
-import java.util.List;
-import java.util.Optional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserChallengeJpaRepository extends JpaRepository<UserChallengeEntity, Long> {
 
-    List<UserChallengeEntity> findAllByUserEntity_UserId(String userId);
-    
-    @EntityGraph(attributePaths = {"challengeEntity"})
-    Page<UserChallengeEntity> findAllByUserEntity_UserId(String userId, Pageable pageable);
-
-    List<UserChallengeEntity> findAllByUserEntity_UserIdInAndChallengeStageEntity_ChallengeEntity_ChallengeId(List<String> userIds,
-        List<Long> challengeIds);
-
-    Optional<UserChallengeEntity> findByUserEntity_UserIdAndChallengeEntity_ChallengeId(String userId,
-        Long challengeId);
-
-    boolean existsByUserEntity_UserIdAndChallengeEntity_ChallengeId(String userId, Long challengeId);
-} 
+}
