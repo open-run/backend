ef6ba08dd85428241526d127cc4b5eaa9ea5831b
Dong Hwan Kim <karmaddee@gmail.com>
2025-12-02 23:31:55 +0900

    BE-87, feat : add Repetitive challenge endpoints

--- src/main/java/io/openur/domain/challenge/controller/ChallengeController.java
+++ src/main/java/io/openur/domain/challenge/controller/ChallengeController.java
@@ -23,7 +23,8 @@ public class ChallengeController {
     private final ChallengeService challengeService;
 
     @GetMapping("/general")
-    public ResponseEntity<PagedResponse<GeneralChallengeDto>> getGeneralChallengeList(
+    public ResponseEntity<PagedResponse<GeneralChallengeDto>>
+    getGeneralChallengeList(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
         @RequestParam(required = false, defaultValue = "0") int page,
         @RequestParam(required = false, defaultValue = "10") int limit
@@ -40,7 +41,8 @@ public class ChallengeController {
     }
 
     @GetMapping("/completed")
-    public ResponseEntity<PagedResponse<GeneralChallengeDto>> getCompletedChallengeList(
+    public ResponseEntity<PagedResponse<GeneralChallengeDto>>
+    getCompletedChallengeList(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
         @RequestParam(required = false, defaultValue = "0") int page,
         @RequestParam(required = false, defaultValue = "10") int limit
@@ -57,21 +59,21 @@ public class ChallengeController {
     }
 
     //TODO : 도전과제 테이블을 변경 우선해 수행
-    @GetMapping("/continuous")
-    public ResponseEntity<PagedResponse<Void>> getContinousChallengeList(
-        @RequestParam(required = false, defaultValue = "") CompletedType type,
+    @GetMapping("/repetitive")
+    public ResponseEntity<PagedResponse<GeneralChallengeDto>>
+    getRepetitiveChallengeList(
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
         @RequestParam(required = false, defaultValue = "0") int page,
         @RequestParam(required = false, defaultValue = "10") int limit
     ) {
         Pageable pageable = PageRequest.of(page, limit);
 
-//        Page<GeneralChallengeDto> challenges = challengeService.getGeneralChallengeList(
-//            null, type, pageable
-//        );
+        Page<GeneralChallengeDto> challenges = challengeService.getRepetitiveChallengeList(
+            userDetails, pageable
+        );
 
-//        return ResponseEntity.ok(
-//            PagedResponse.build(challenges, "success")
-//        );
-        return null;
+        return ResponseEntity.ok(
+            PagedResponse.build(challenges, "success")
+        );
     }
 }
