90bc004e6ef14448f3a0e81750c5716fdd407a58
Dong Hwan Kim <karmaddee@gmail.com>
2025-12-14 22:41:58 +0900

    BE-87, chore : exception for challenge methods

---
+++ src/main/java/io/openur/domain/challenge/exception/ChallengeNotAssignedException.java
@@ -0,0 +1,8 @@
+package io.openur.domain.challenge.exception;
+
+public class ChallengeNotAssignedException extends RuntimeException {
+
+    public ChallengeNotAssignedException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/io/openur/domain/challenge/exception/ChallengeStageInvalid.java
@@ -0,0 +1,8 @@
+package io.openur.domain.challenge.exception;
+
+public class ChallengeStageInvalid extends RuntimeException {
+
+    public ChallengeStageInvalid(String message) {
+        super(message);
+    }
+}
