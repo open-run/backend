e4662aad852e86fc2ce94f14e63afb5020f49206
Yejin Kelly Joo <yejinkellyjoo@gmail.com>
2026-01-11 23:02:17 +0100

    BE-98 feat: update NFTContract to match current version
    - Updated NFTContract to support batch minting of tokens and added safe transfer methods.
    
    Signed-off-by: Yejin Kelly Joo <yejinkellyjoo@gmail.com>

--- src/main/java/io/openur/domain/NFT/contract/NFTContract.java
+++ src/main/java/io/openur/domain/NFT/contract/NFTContract.java
@@ -3,11 +3,13 @@ package io.openur.domain.NFT.contract;
 import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Function;
 import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.DynamicArray;
+import org.web3j.abi.datatypes.DynamicBytes;
 import org.web3j.abi.datatypes.Utf8String;
 import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.tx.Contract;
 import org.web3j.protocol.Web3j;
-import org.web3j.crypto.Credentials;
+import org.web3j.tx.TransactionManager;
 import org.web3j.tx.gas.ContractGasProvider;
 import org.web3j.protocol.core.RemoteCall;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
@@ -15,43 +17,81 @@ import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class NFTContract extends Contract {
     public static final String BINARY = "";
 
-    protected NFTContract(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider gasProvider) {
-        super(BINARY, contractAddress, web3j, credentials, gasProvider);
+    protected NFTContract(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider gasProvider) {
+        super(BINARY, contractAddress, web3j, transactionManager, gasProvider);
     }
 
-    public static NFTContract load(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider gasProvider) {
-        return new NFTContract(contractAddress, web3j, credentials, gasProvider);
+    public static NFTContract load(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider gasProvider) {
+        return new NFTContract(contractAddress, web3j, transactionManager, gasProvider);
     }
 
-    // NFT 민팅
-    public RemoteCall<TransactionReceipt> mintItemForTask(String to, BigInteger taskId, BigInteger amount) {
+    public RemoteCall<TransactionReceipt> mintToken(String to, BigInteger tokenId, BigInteger amount) {
         final Function function = new Function(
-            "mintItemForTask",
-            Arrays.asList(new Address(to), new Uint256(taskId), new Uint256(amount)),
+            "mintToken",
+            Arrays.asList(new Address(to), new Uint256(tokenId), new Uint256(amount)),
             Collections.emptyList()
         );
         return executeRemoteCallTransaction(function);
     }
 
-    // baseURI 설정 함수
-    public RemoteCall<TransactionReceipt> setBaseURI(String uri) {
+    public RemoteCall<TransactionReceipt> mintBatchTokens(String to, List<BigInteger> tokenIds, List<BigInteger> amounts) {
+        DynamicArray<Uint256> tokenIdsArray = new DynamicArray<>(Uint256.class, 
+            tokenIds.stream().map(Uint256::new).toList());
+        DynamicArray<Uint256> amountsArray = new DynamicArray<>(Uint256.class,
+            amounts.stream().map(Uint256::new).toList());
+        
+        final Function function = new Function(
+            "mintBatchTokens",
+            Arrays.asList(new Address(to), tokenIdsArray, amountsArray),
+            Collections.emptyList()
+        );
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<TransactionReceipt> setBaseURI(String newBaseURI) {
         final Function function = new Function(
             "setBaseURI",
-            Arrays.asList(new Utf8String(uri)),
+            Arrays.asList(new Utf8String(newBaseURI)),
             Collections.emptyList()
         );
         return executeRemoteCallTransaction(function);
     }
 
-    //mint를 위해 taskID set
-    public RemoteCall<TransactionReceipt> setTaskItem(BigInteger taskId, BigInteger itemId) {
+    public RemoteCall<TransactionReceipt> safeTransferToken(String from, String to, BigInteger tokenId, BigInteger amount, byte[] data) {
         final Function function = new Function(
-            "setTaskItem",
-            Arrays.asList(new Uint256(taskId), new Uint256(itemId)),
+            "safeTransferToken",
+            Arrays.asList(
+                new Address(from),
+                new Address(to),
+                new Uint256(tokenId),
+                new Uint256(amount),
+                new DynamicBytes(data)
+            ),
+            Collections.emptyList()
+        );
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<TransactionReceipt> safeTransferBatchTokens(String from, String to, List<BigInteger> tokenIds, List<BigInteger> amounts, byte[] data) {
+        DynamicArray<Uint256> tokenIdsArray = new DynamicArray<>(Uint256.class,
+            tokenIds.stream().map(Uint256::new).toList());
+        DynamicArray<Uint256> amountsArray = new DynamicArray<>(Uint256.class,
+            amounts.stream().map(Uint256::new).toList());
+        
+        final Function function = new Function(
+            "safeTransferBatchTokens",
+            Arrays.asList(
+                new Address(from),
+                new Address(to),
+                tokenIdsArray,
+                amountsArray,
+                new DynamicBytes(data)
+            ),
             Collections.emptyList()
         );
         return executeRemoteCallTransaction(function);
