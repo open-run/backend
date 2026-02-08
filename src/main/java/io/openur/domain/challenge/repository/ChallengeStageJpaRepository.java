881fa5ca76a4eff1207b8e6392c677fc47b5db1d
Dong Hwan Kim <karmaddee@gmail.com>
2026-02-08 17:13:26 +0900

    BE-87, feat : add missing new challenge stage issuer

--- src/main/java/io/openur/domain/challenge/repository/ChallengeStageJpaRepository.java
+++ src/main/java/io/openur/domain/challenge/repository/ChallengeStageJpaRepository.java
@@ -2,10 +2,13 @@ package io.openur.domain.challenge.repository;
 
 import io.openur.domain.challenge.entity.ChallengeStageEntity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChallengeStageJpaRepository extends
     JpaRepository<ChallengeStageEntity, Long>
 {
     List<ChallengeStageEntity> findAllByChallengeEntityChallengeIdOrderByStageNumberAsc(Long challengeId);
+
+    Optional<ChallengeStageEntity> findFirstByChallengeEntity_ChallengeIdAndStageNumberIsGreaterThan(Long challengeId, Integer stage);
 }
